a80041da69a4a9b5e1b4c39adfe1af50eb034a9e
Mohnish Kumar <90864490+MohnishK7@users.noreply.github.com>
2023-09-22 17:37:45 +0530

    Create Petya _and_Strings.cpp

---
+++ CF_Ladder/Petya _and_Strings.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #include "ext/pb_ds/assoc_container.hpp"
+// #include "ext/pb_ds/tree_policy.hpp"
+// Header Files End
+#define F first
+#define S second
+#define all(X) (X).begin(), (X).end()
+#define REP(i,a,b) for (int i = a; i<b; i++)
+#define S second
+#define ii pair<int, int>
+#define PB push_back
+#define MP make_pair
+#define SQ(a) (a)*(a)
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define endl '\n'
+// policy based data structure
+// using namespace __gnu_pbds;
+// template <class T>
+//  using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//  template <class T>
+//  using omset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+
+// void solve() {
+//    // my code
+
+// }
+const int NUMS = 26;
+
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+   //input.txt
+   freopen("input1.txt", "r", stdin);
+   //output.txt
+   freopen("output1.txt", "w", stdout);
+#endif
+   ios_base::sync_with_stdio(false);
+   cin.tie(0);
+
+   // Petya and Strings
+   string s1;
+   cin >> s1;
+   string s2;
+   cin >> s2;
+
+   // convert uppercase to lowercase
+   for (int i = 0; i < s1.size(); i++) {
+      if (s1[i] < 92) {
+         s1 += 32;
+      }
+      if (s2[i] < 92) {
+         s2[i] += 32;
+      }
+   }
+   if (s1 < s2) {
+      cout << -1 << '\n';
+   }
+   if (s2 < s1) {
+      cout << 1 << '\n';
+   }
+   if (s1 == s2) {
+      cout << 0 << '\n';
+   }
+}
