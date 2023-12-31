1b3eac8b2d9b1822d0f1f5c8a591a5996e006a25
Mohnish Kumar <90864490+MohnishK7@users.noreply.github.com>
2023-12-31 13:26:55 +0530

    Create Stones on the table.cpp

---
+++ CF_Ladder/ACD_Ladder_Grind_800/Stones on the table.cpp
@@ -0,0 +1,86 @@
+//In the name of God
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+
+int main() {
+#ifndef ONLINE_JUDGE
+   //input.txt
+   freopen("input1.txt", "r", stdin);
+   //output.txt
+   freopen("output1.txt", "w", stdout);
+   //error
+   freopen("error.txt", "w", stderr);
+#endif
+   ios_base::sync_with_stdio(false);
+   cin.tie(0);
+
+   // code here
+   // not my solution
+   int n;
+   cin >> n;
+   string s;
+   cin >> s;
+   int r = 0;
+   int g = 0;
+   int b = 0;
+   for (int i = 0; i < n; i++) {
+      if (s[i] == s[i + 1])
+         if (s[i] == 'R') {
+            ++r;
+         }
+         else if (s[i] == 'G') {
+            ++g;
+         }
+         else if (s[i] == 'B') {
+            ++b;
+         }
+   }
+   cout << r + g + b;
+   return 0;
+}
